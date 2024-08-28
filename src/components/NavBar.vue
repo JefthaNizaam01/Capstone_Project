a58e00b251b4d7c2cde5ac1b2e4fb423247f540f
Nizaam Jeftha <nizaam01.jeftha@gmail.com>
2024-08-28 10:19:39 +0200

    navbarupdate

---
+++ src/components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+    <nav class="navbar navbar-expand-lg mt-0 fixed-top">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img class="img" src="https://jefthanizaam01.github.io/favoriteimages/images/NJJ.png" alt="logo" loading="lazy"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#education">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#skills">Checkout</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#experience">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#projects">Logout</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#testimonials">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#reachMe">Reach me</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style >
+
+</style>
